7c12fc5926c0e7e8e15cd2322da792961b6bfaab
Tiago Nascimento <tnascimento@salesforce.com>
2020-05-13 12:51:48 -0300

    initial version

---
+++ index.js
@@ -0,0 +1,16 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+try {
+    const type = core.getInput('type');
+    console.log('Type of deployment: ${type}!');
+
+    const clientId = core.getInput('client_id');
+    console.log('ClientId to be used to connect in the destionation org: ${clientId}');
+
+    const username = core.getIinput('username');
+    console.log('Username to be used to connect in the destination org: ${username}');
+  
+} catch (error) {
+    core.setFailed(error.message);
+}
