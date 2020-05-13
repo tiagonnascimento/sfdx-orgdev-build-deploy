2ce953631f0f17795e30c51eb77f3db967a60292
Tiago Nascimento <tnascimento@salesforce.com>
2020-05-13 15:20:03 -0300

    update js to test script

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const github = require('@actions/github');
 
 try {
     const type = core.getInput('type');
-    console.log('Type of deployment: ${type}!');
+    console.log('Type of deployment: ' + type);
 
     const clientId = core.getInput('client_id');
-    console.log('ClientId to be used to connect in the destionation org: ${clientId}');
+    console.log('ClientId to be used to connect in the destionation org: ' + clientId);
 
     const username = core.getInput('username');
-    console.log('Username to be used to connect in the destination org: ${username}');
+    console.log('Username to be used to connect in the destination org: ' + username);
   
 } catch (error) {
     core.setFailed(error.message);
