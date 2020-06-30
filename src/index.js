b44336f1cd9f57e7758e898dffd8e204daba0db4
CircleCi <manuel.cortes@salesforce.com>
2020-06-30 11:18:43 -0500

    Mejoras IC

--- src/index.js
+++ src/index.js
@@ -8,20 +8,7 @@ try {
   core.debug("=== index.js ===");
   core.debug(`The event payload: ${payload}`);
   //dependencies.install();
-  /*sfdx.login(
-    {
-      certificatePath : "devops/server.key.enc", 
-      decryptionKey : "8CB2237D0679CA88DB6464EAC60DA96345513964D4F5EC90C4B6A22B810B337F", 
-      decryptionIV : "E1786DC9EC34036F6DA24067A92E39CA"
-    }, 
-    {
-      clientId : "3MVG99E3Ry5mh4zo9bk2v8pC2ros2NQz1pjy8v2VdlqBfQWKtT2c1uRmw92WL1MzPdQISSlx8CFoOXJq7J9Lq",
-      orgType : "sandbox",
-      username : "manuel.cortes@bancoestado.cl.deva"
-    }
-  );
-
-  */
+  
 
  
  
