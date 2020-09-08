a78a4157fac7a96eb82446f4d8e1565275d25157
Tiago Nascimento <tnascimento@salesforce.com>
2020-09-08 13:20:38 -0300

    changing Salesforce CLI version to use the previous released instead of latest

--- dist/index.js
+++ dist/index.js
@@ -16542,7 +16542,8 @@ var fnInstallSFDX = function(){
     execCommand.run('wget', ['https://developer.salesforce.com/media/salesforce-cli/sfdx-cli/channels/stable/sfdx-cli-v7.72.0-697e9faee2-linux-x64.tar.xz']);
     //execCommand.run('wget', ['https://developer.salesforce.com/media/salesforce-cli/sfdx-linux-amd64.tar.xz']);
     execCommand.run('mkdir', ['-p', 'sfdx-cli']);
-    execCommand.run('tar', ['xJf', 'sfdx-linux-amd64.tar.xz', '-C', 'sfdx-cli', '--strip-components', '1']);
+    execCommand.run('tar', ['xJf', 'sfdx-cli-v7.72.0-697e9faee2-linux-x64.tar.xz', '-C', 'sfdx-cli', '--strip-components', '1']);
+    //execCommand.run('tar', ['xJf', 'sfdx-linux-amd64.tar.xz', '-C', 'sfdx-cli', '--strip-components', '1']);
     execCommand.run('./sfdx-cli/install', []);
     core.info('=== SFDX cli installed ===');
 };
