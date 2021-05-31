4a9f308371238589e77ce31d3f8291c2ccef981c
Roger <rogmarq@gmail.com>
2021-05-31 18:03:58 -0300

    changed name

--- src/utils/exec-async-command.js
+++ src/utils/exec-async-command.js
@@ -2,22 +2,22 @@ const core = require('@actions/core')
 const { spawn } = require('child_process');
 
 module.exports.run = function(command, args, workingFolder = null) {
-    var extraParams = {};
+    const extraParams = {};
     if (workingFolder) {
         extraParams.cwd = workingFolder;
     }
     extraParams.encoding = 'utf-8';
     extraParams.maxBuffer = 1024 * 1024 * 10
 
-    var spawn = spawn(command, args, extraParams);
+    const ls = spawn(command, args, extraParams);
 
-    spawn.stdout.on('data', (data) => {
+    ls.stdout.on('data', (data) => {
         core.info("Command executed: " + command)
         core.info("With the following args: " + args.toString());
         core.info("Having the following return: " + data.toString());
     });
 
-    spawn.stderr.on('data', (data) => {
+    ls.stderr.on('data', (data) => {
         core.error(data.toString());
         throw Error(data.toString());
     });
