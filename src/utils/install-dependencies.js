6e0ccd896f096ca7f073c5d1ca67e686f4ffc6b7
Remi Pichon <3901946+remipichon@users.noreply.github.com>
2023-06-26 11:29:26 +0200

    update download package url

--- src/utils/install-dependencies.js
+++ src/utils/install-dependencies.js
@@ -4,7 +4,7 @@ const execCommand = require('./exec-command.js');
 var fnInstallSFDX = function(){
     core.info('=== Downloading and installing SFDX cli ===');
     //execCommand.run('wget', ['https://developer.salesforce.com/media/salesforce-cli/sfdx-cli/channels/stable/sfdx-cli-v7.72.0-697e9faee2-linux-x64.tar.xz']);
-    execCommand.run('wget', ['https://developer.salesforce.com/media/salesforce-cli/sfdx-linux-amd64.tar.xz']);
+    execCommand.run('wget', ['https://developer.salesforce.com/media/salesforce-cli/sfdx/channels/stable/sfdx-linux-x64.tar.gz']);
     execCommand.run('mkdir', ['-p', 'sfdx-cli']);
     //execCommand.run('tar', ['xJf', 'sfdx-cli-v7.72.0-697e9faee2-linux-x64.tar.xz', '-C', 'sfdx-cli', '--strip-components', '1']);
     execCommand.run('tar', ['xJf', 'sfdx-linux-amd64.tar.xz', '-C', 'sfdx-cli', '--strip-components', '1']);
